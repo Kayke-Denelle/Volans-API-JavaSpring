feb03f896354eb85d0791f830b8a8436c08057b7
Kayke_Denelle <kaykedenelle@gmail.com>
2025-04-27 02:15:13 -0300

    |Cors

---
+++ backend/src/main/java/com/example/backend/Security/Config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.backend.Security.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica para todas as rotas
+                        .allowedOrigins("http://127.0.0.1:5500") // Permite seu front rodando no Live Server
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Cabeçalhos permitidos
+                        .allowCredentials(true); // Se precisar enviar cookies ou auth
+            }
+        };
+    }
+}
