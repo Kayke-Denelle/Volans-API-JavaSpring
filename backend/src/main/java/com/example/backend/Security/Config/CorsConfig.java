bd2cec283441fae3f5cafc1f1c5e96eac12d44d8
Kayke_Denelle <kaykedenelle@gmail.com>
2025-04-28 16:12:33 -0300

    1

--- backend/src/main/java/com/example/backend/Security/Config/CorsConfig.java
+++ backend/src/main/java/com/example/backend/Security/Config/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Aplica para todas as rotas
-                        .allowedOrigins("http://127.0.0.1:5500") // Permite seu front rodando no Live Server
+                        .allowedOrigins("http://127.0.0.1:5500") 
+                        .allowedOrigins("https://volans-api-javaspring.onrender.com")// Permite seu front rodando no Live Server
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                         .allowedHeaders("*") // Cabeçalhos permitidos
                         .allowCredentials(true); // Se precisar enviar cookies ou auth
